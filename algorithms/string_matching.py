50589f2f21b36d1ea0cfec1de1ff74440b252a84
Maximo Ramirez <a00839731@tec.mx>
2025-09-24 11:07:08 -0600

    feat: add KMP string matching algorithm and failure table

---
+++ algorithms/string_matching.py
@@ -0,0 +1,42 @@
+def kmp_search_all_occurrences(text, pattern): # Retorna un generador (Convertir en lista al recibirlo)
+    n = len(text)
+    m = len(pattern)
+
+    i,j = 0,0
+
+    failure_table = build_failure_table(pattern)
+
+    while i < n:
+        if text[i] == pattern[j]:
+            i += 1
+            j += 1
+        else:
+            if j > 0:
+                j = failure_table[j-1]
+            else:
+                i += 1
+        if j == m:
+            yield i - m
+            j = failure_table[j-1]
+    
+
+def build_failure_table(pattern):
+    """Construye tabla de fallos para KMP"""
+    n = len(pattern)
+    lps = [0] * n
+
+    length = 0
+    i = 1
+
+    while i < n:
+        if pattern[i] == pattern[length]:
+            length += 1
+            lps[i] = length
+            i += 1
+        else:
+            if length != 0:
+                length = lps[length - 1]
+            else:
+                lps[i] = 0
+                i += 1
+    return lps
