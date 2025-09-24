e6e25c0898d8c29b204e29ad67e85fc1cdd311ce
Maximo Ramirez <a00839731@tec.mx>
2025-09-24 11:12:18 -0600

    feat: add main application entry point
    - Create main.py as central execution file

---
+++ main.py
@@ -0,0 +1,16 @@
+from modules.file_handler import load_all_files
+from modules.gene_finder import find_genes_index
+
+# PUNTO 1: Búsqueda de genes en genoma
+
+if __name__ == "__main__":
+    print("===Analisis Genomico de SARS-COV-2===\n")
+
+    print("Cargando archivos...")
+    files_data = load_all_files()
+    if files_data:
+        print("Archivos cargados exitosamente!\n")
+
+    print("---PUNTO 1: Busqueda de genes en genoma---")
+    
+    find_genes_index(files_data['genome'], files_data['genes'])
